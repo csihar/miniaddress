136443dbe64dca0c51e1df00a091f0595396a9e8
csihar <ksanderspdx@gmail.com>
2013-08-29 15:23:54 -0700

    added delete_house command

---
+++ miniaddress/management/commands/delete_house.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+from miniaddress.models import House, Owner
+from miniaddress.functions import check_existence
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--addr-contains',
+            action='store',
+            type='string',
+            dest='addr-contains',
+            default='',
+            help="Part of address of the house to delete"),
+        )    
+    
+    def handle(self, *args, **options):
+        ''' Delete address with --addr-contains=foo '''
+        if options['addr-contains']:
+            address = options['addr-contains']
+            del_house = House.objects.filter(address__icontains=address)
+            if not del_house:
+                self.stdout.write('No houses containing [%s]' % address )
+            for house in del_house:
+                address = house.address
+                owner = house.owner
+                house.delete()
+                self.stdout.write('Deleted house: address=[%s] owner=[%s]\n' % (address, owner) )
+                owner_has_house = House.objects.filter(owner__name=owner)
+                if not owner_has_house:
+                    del_owner = Owner.objects.get(name=owner)
+                    del_owner.delete()
+                    self.stdout.write('Deleted owner: name=[%s]\n' % owner )
+        else:
+            raise CommandError('Please specify an address')
+
--- readme.txt
+++ readme.txt
@@ -1,9 +1,15 @@
 miniaddress: a simple command-line address manager.
 
+
 Extra credit completed:
---- Wed 8/28/13:
+
+---- Wed 8/28/2013:
 6.   Store the code in git, and either send the zipped git repo, or a link or directions how to clone it.
 7.   Store the code in git and allow me to track your progress via git.  After you create the repository, give me directions how to clone it, and then make periodic commits as you work on the project.
 12.  Enable the Django admin for House and Owner so that an admin can create/read/update/delete houses and owners.
 13.  Create a web page to show all the houses and owners.
 14.  Add a date_created field to house and owner that is automatically set to the date/time when they were created.
+
+---- Thu 8/29/2013:
+1.   Implement a delete_house command.
+2.   For the delete_house command, when deleting a house, also delete the owner if they own no other houses.
